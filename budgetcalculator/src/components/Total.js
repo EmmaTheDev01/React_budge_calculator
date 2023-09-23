6525dc6d73766a4135ac6e6ddc6ac511bf4bccf4
EmmaTheDev01 <worldoffictionrw@gmail.com>
2023-09-23 12:11:42 +0300

    Initial commit

---
+++ budgetcalculator/src/components/Total.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../style/style.css'
+const Total = () => {
+  return (
+    <div className='total'>
+        <div className='income'>
+            <h5>Income</h5>
+            <p>$0</p>
+        </div>
+        <div className='expenses'>
+            <h5>Expenses</h5>
+            <p>$0</p>
+        </div>
+    </div>
+  )
+}
+
+export default Total
---
+++ budgetcalculator/src/components/Transactions.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../style/style.css";
+const Transactions = () => {
+  return (
+    <div className="transactions">
+      <div className="tile">
+        <div className="name">
+          <p>Car</p>
+        </div>
+        <div className="amount">
+          <p>$230</p>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Transactions;
